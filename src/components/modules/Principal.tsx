bd8aff3a62f04bdf7d7356b239bb1de15516545f
emmajane1313 <admin@digitalax.xyz>
2024-07-23 06:56:00 -0400

    اخر

--- src/components/modules/Principal.tsx
+++ src/components/modules/Principal.tsx
@@ -5,14 +5,25 @@ import { FunctionComponent } from "react";
 
 const Principal: FunctionComponent = (): JSX.Element => {
   return (
-    <div className="relative w-full h-[150rem] flex items-start justify-start bg-black">
-      <Image
-        layout="fill"
-        draggable={false}
-        src={`${INFURA_GATEWAY}/ipfs/QmNrybajbY5LhWDTLsAk86PWgigHKdu8DdSoX8RmbbAq75`}
-        objectFit="cover"
-        priority
-      />
+    <div className="relative w-full h-fit flex items-start justify-start bg-black flex-col">
+      <div className="relative w-full h-[150rem] flex items-start justify-start ">
+        <Image
+          layout="fill"
+          draggable={false}
+          src={`${INFURA_GATEWAY}/ipfs/QmNrybajbY5LhWDTLsAk86PWgigHKdu8DdSoX8RmbbAq75`}
+          objectFit="cover"
+          priority
+        />
+      </div>
+      <div className="relative w-full h-[200rem] flex items-start justify-start ">
+        <Image
+          layout="fill"
+          draggable={false}
+          src={`${INFURA_GATEWAY}/ipfs/QmUyn9D92yEjz4ES7GyYAoBz7jgCUcFwQTxXmJmJeBNEwk`}
+          objectFit="cover"
+          priority
+        />
+      </div>
     </div>
   );
 };
