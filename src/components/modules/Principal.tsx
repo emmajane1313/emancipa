e24ca3f35cf736c8a135c6549c41ba56c6f58a9f
emmajane1313 <admin@digitalax.xyz>
2024-07-24 00:28:31 -0400

    pegatinas trans

--- src/components/modules/Principal.tsx
+++ src/components/modules/Principal.tsx
@@ -24,6 +24,44 @@ const Principal: FunctionComponent = (): JSX.Element => {
           priority
         />
       </div>
+      <div className="relative w-full h-fit flex items-start justify-start flex-row">
+        {[
+          "QmRt6dpmTD7Ps42u3fBH2trJM4giVURGsL8XwwgBJuMUVn",
+          "QmeJXfTzbe8qquHW8yrFYSHMtAiMgLcaab6f5qNG6j5bzX",
+          "QmVbzcttHWyRzu8ef2tLWkzKhAaoAo2PZwoAy7qyZFxExe",
+        ].map((imagen: string, indice: number) => {
+          return (
+            <div key={indice} className="relative flex w-full h-[30rem]">
+              <Image
+                src={`${INFURA_GATEWAY}/ipfs/${imagen}`}
+                layout="fill"
+                objectFit="cover"
+                draggable={false}
+                priority
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="relative w-full h-fit flex items-start justify-start flex-row">
+        {[
+          "QmdayoVLF4z7F1zufPDusmFbHRSUbvGoZLB7hS9xznSdmR",
+          "QmZk7DFZhz9Y2JrfKJmUEpMmk8tfGkeaAHGUjSA1Ysquhh",
+          "QmZvRmxFdKE3zvdky62aCK65MXFfEyvfLbX9gG8ivKebiM",
+        ].map((imagen: string, indice: number) => {
+          return (
+            <div key={indice} className="relative flex w-full h-[30rem]">
+              <Image
+                src={`${INFURA_GATEWAY}/ipfs/${imagen}`}
+                layout="fill"
+                objectFit="cover"
+                draggable={false}
+                priority
+              />
+            </div>
+          );
+        })}
+      </div>
       <div className="relative w-full h-[130rem] flex items-start justify-start ">
         <Image
           layout="fill"
@@ -33,9 +71,6 @@ const Principal: FunctionComponent = (): JSX.Element => {
           priority
         />
       </div>
-
-
-      
     </div>
   );
 };
