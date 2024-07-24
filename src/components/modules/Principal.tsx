9e7a87b25a0da582c0e599f64fe469221725acbc
emmajane1313 <admin@digitalax.xyz>
2024-07-24 06:27:03 -0400

    Responsividad

--- src/components/modules/Principal.tsx
+++ src/components/modules/Principal.tsx
@@ -6,22 +6,26 @@ import { FunctionComponent } from "react";
 const Principal: FunctionComponent = (): JSX.Element => {
   return (
     <div className="relative w-full h-fit flex items-start justify-start bg-black flex-col">
-      <div className="relative w-full h-[150rem] flex items-start justify-start ">
+      <div className="relative w-full">
         <Image
-          layout="fill"
+          layout="responsive"
           draggable={false}
           src={`${INFURA_GATEWAY}/ipfs/QmNrybajbY5LhWDTLsAk86PWgigHKdu8DdSoX8RmbbAq75`}
-          objectFit="cover"
+          objectFit="contain"
           priority
+          width={3038}
+          height={4696}
         />
       </div>
-      <div className="relative w-full h-[200rem] flex items-start justify-start ">
+      <div className="relative w-full">
         <Image
-          layout="fill"
+          layout="responsive"
           draggable={false}
           src={`${INFURA_GATEWAY}/ipfs/QmUyn9D92yEjz4ES7GyYAoBz7jgCUcFwQTxXmJmJeBNEwk`}
-          objectFit="cover"
+          objectFit="contain"
           priority
+          width={2698}
+          height={5503}
         />
       </div>
       <div className="relative w-full h-fit flex items-start justify-start flex-row">
@@ -31,13 +35,15 @@ const Principal: FunctionComponent = (): JSX.Element => {
           "QmVbzcttHWyRzu8ef2tLWkzKhAaoAo2PZwoAy7qyZFxExe",
         ].map((imagen: string, indice: number) => {
           return (
-            <div key={indice} className="relative flex w-full h-[30rem]">
+            <div key={indice} className="relative w-full">
               <Image
                 src={`${INFURA_GATEWAY}/ipfs/${imagen}`}
-                layout="fill"
-                objectFit="cover"
                 draggable={false}
+                layout="responsive"
+                objectFit="contain"
                 priority
+                width={1025}
+                height={1025}
               />
             </div>
           );
@@ -50,34 +56,40 @@ const Principal: FunctionComponent = (): JSX.Element => {
           "QmZvRmxFdKE3zvdky62aCK65MXFfEyvfLbX9gG8ivKebiM",
         ].map((imagen: string, indice: number) => {
           return (
-            <div key={indice} className="relative flex w-full h-[30rem]">
+            <div key={indice} className="relative w-full">
               <Image
                 src={`${INFURA_GATEWAY}/ipfs/${imagen}`}
-                layout="fill"
-                objectFit="cover"
                 draggable={false}
+                layout="responsive"
+                objectFit="contain"
                 priority
+                width={1025}
+                height={1025}
               />
             </div>
           );
         })}
       </div>
-      <div className="relative w-full h-[130rem] flex items-start justify-start ">
+      <div className="relative w-full">
         <Image
-          layout="fill"
+          layout="responsive"
           draggable={false}
           src={`${INFURA_GATEWAY}/ipfs/QmNNS4wKZaXggt9K4Ww6RKzTpjHJLxS7Vr5LmNA5akeTJ1`}
-          objectFit="cover"
+          objectFit="contain"
           priority
+          width={896}
+          height={1360}
         />
       </div>
-      <div className="relative w-full h-[130rem] flex items-start justify-start ">
+      <div className="relative w-full">
         <Image
-          layout="fill"
+          layout="responsive"
           draggable={false}
           src={`${INFURA_GATEWAY}/ipfs/QmSscfUkSXxqRPAeC3VURKiiWj5sbfdwSoeDHUiwefjGdF`}
-          objectFit="cover"
+          objectFit="contain"
           priority
+          width={960}
+          height={1344}
         />
       </div>
     </div>
