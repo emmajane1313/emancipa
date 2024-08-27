0b0cd607af3f15909ca853289f060c64e6487f4d
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2024-08-27 17:36:10 -0400

    pj

---
+++ next.config.js
@@ -0,0 +1,52 @@
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  experimental: {
+    scrollRestoration: true,
+    optimizeCss: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "thedial.infura-ipfs.io",
+        pathname: "/ipfs/**",
+      },
+    ],
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
+  async headers() {
+    let headersConfig = [];
+
+    const allowedOrigins = ["https://thedial.infura-ipfs.io"];
+    allowedOrigins.forEach((origin) => {
+      headersConfig.push({
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: origin,
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+        ],
+      });
+    });
+
+    return headersConfig;
+  },
+};
+
+module.exports = nextConfig;
