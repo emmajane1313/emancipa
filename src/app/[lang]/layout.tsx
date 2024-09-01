53affa162039c1297aae79d15134f6da1b6df81b
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2024-09-01 15:59:29 -0400

    sin head

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import "./../../globals.css";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.emancipa.xyz"),
@@ -21,6 +20,12 @@ export const metadata: Metadata = {
     title: "Emancipa",
     description:
       "We will not be banned. Todos los géneros, todos los amores, todas las vidas, no seremos prohibidos. Women will not stay banned. زن زندگی آزادی",
+  },
+  robots: {
+    googleBot: {
+      index: true,
+      follow: true,
+    },
   },
   keywords: "Web3, Web3 Fashion, Moda Web3, Open Source, CC0, Emma-Jane MacKinnon-Lee, Open Source LLMs, DIGITALAX, F3Manifesto, www.digitalax.xyz, www.f3manifesto.xyz, Women, Life, Freedom."
 };
@@ -48,26 +53,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang={params.lang}>
-      <Head>
-        <meta
-          name="keywords"
-          content="Web3, Web3 Fashion, Moda Web3, Open Source, CC0, Emma-Jane MacKinnon-Lee, Open Source LLMs, DIGITALAX, F3Manifesto, www.digitalax.xyz, www.f3manifesto.xyz, Women, Life, Freedom."
-        />
-        <meta name="robots" content="index, follow" />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "WebSite",
-              name: "Emancipa",
-              description:
-                "Web3, Web3 Fashion, Moda Web3, Open Source, CC0, Emma-Jane MacKinnon-Lee, Open Source LLMs, DIGITALAX, F3Manifesto, www.digitalax.xyz, www.f3manifesto.xyz, Women, Life, Freedom.",
-              url: "https://www.emancipa.xyz/",
-            }),
-          }}
-        ></script>
-      </Head>
       <body>{children}</body>
     </html>
   );
