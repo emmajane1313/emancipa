7559a7bec075a93edba25dcdbdb0596a1336b919
emmajane1313 <admin@digitalax.xyz>
2024-09-25 11:23:36 -0400

    hu tu ym

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -41,6 +41,9 @@ export async function generateStaticParams() {
     { lang: "fr" },
     { lang: "yi" },
     { lang: "ja" },
+    { lang: "ym" },
+    { lang: "hu" },
+    { lang: "tu" },
   ];
 }
 
