a7d94ba986b85b9806720977f41aa169abd317a6
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-12 16:35:19 -0400

    mainnet

--- src/app/[lang]/dictionaries.js
+++ src/app/[lang]/dictionaries.js
@@ -11,7 +11,7 @@ const dictionaries = {
   yi: () => import("./dictionaries/yi.json").then((module) => module.default),
   fr: () => import("./dictionaries/fr.json").then((module) => module.default),
   ja: () => import("./dictionaries/ja.json").then((module) => module.default),
-  tu: () => import("./dictionaries/tr.json").then((module) => module.default),
+  tr: () => import("./dictionaries/tr.json").then((module) => module.default),
   hu: () => import("./dictionaries/hu.json").then((module) => module.default),
   ym: () => import("./dictionaries/ym.json").then((module) => module.default),
 };
