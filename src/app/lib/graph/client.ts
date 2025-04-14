b3297eb63ae182e303fccb5e4d52613a6c0f242c
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-14 01:19:01 -0400

    graph

--- src/app/lib/graph/client.ts
+++ src/app/lib/graph/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const emancipaLink = new HttpLink({
-  uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/BC2P8EzxwzRcFKkHijDGd18YcbiFbr7k4sRBq1bPNaWP`,
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/6cvPKEkFN2Xd6nJrokr4xkUajYK4YRk27RhtTduehemW`,
 });
 
 export const emancipaClient = new ApolloClient({
