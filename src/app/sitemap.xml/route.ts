dc9b0c6ab039b88d1621e181a8e58e8552bc93c3
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-27 17:12:39 -0400

    sitemap

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,17 +1,26 @@
 import { NextResponse } from "next/server";
-import { IMAGE_SET, INFURA_GATEWAY } from "../lib/constantes";
+import { IMAGE_SET, INFURA_GATEWAY } from "../lib/constantes"; 
 
+const locales = [
+  "en",
+  "es",
+  "fr",
+  "fa",
+  "uk",
+  "ar",
+  "pt",
+  "he",
+  "yi",
+  "ja",
+  "hu",
+  "tr",
+  "ym",
+]; 
 export async function GET() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://emancipa.xyz";
 
-  const body = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset 
-  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
-  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
->
-  <url>
-    <loc>${baseUrl}</loc>
-    ${IMAGE_SET.map(
+  const imagesXml = (locale: string) =>
+    IMAGE_SET.map(
       (img) => `
       <image:image>
         <image:loc>${INFURA_GATEWAY}/ipfs/${img.imagen}</image:loc>
@@ -19,8 +28,32 @@ export async function GET() {
         <image:caption><![CDATA[${img.alt} | Emancipa | Emma-Jane MacKinnon-Lee]]></image:caption>
       </image:image>
     `
-    ).join("")}
-  </url>
+    ).join("");
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset 
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml"
+>
+  ${locales
+    .map(
+      (locale) => `
+    <url>
+      <loc>${baseUrl}/${locale}</loc>
+      ${locales
+        .map(
+          (altLocale) => `
+        <xhtml:link rel="alternate" hreflang="${altLocale}" href="${baseUrl}/${altLocale}" />
+        `
+        )
+        .join("")}
+      <xhtml:link rel="alternate" hreflang="x-default" href="${baseUrl}/en" />
+      ${imagesXml(locale)}
+    </url>
+  `
+    )
+    .join("")}
 </urlset>`;
 
   return new NextResponse(body, {
