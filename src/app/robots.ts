d1c227a4f667e2c16ae71bd8b36b642fc9f7f1a9
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-27 02:47:16 -0400

    sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,8 @@
-import { LOCALES } from "@/middleware";
 import { MetadataRoute } from "next";
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
   return {
     rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: LOCALES.map(
-      (lang) => `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
-    ),
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
   };
 }
