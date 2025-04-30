5dd366bb38792f56e2962b3974ab68afd311ade6
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-30 12:42:33 -0400

    opengraph

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,6 +45,7 @@ export function middleware(request: NextRequest) {
     pathname.startsWith("/images") ||
     pathname.startsWith("/fonts") ||
     pathname.startsWith("/favicon.ico") ||
+    pathname.startsWith("/opengraph_image.png") ||
     pathname.startsWith("/api") ||
     pathname.startsWith("/sitemap.xml")
   ) {
@@ -79,5 +80,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next|images|fonts|favicon.ico|api|sitemap).*)"],
+  matcher: ["/((?!_next|images|fonts|opengraph_image.png|favicon.ico|api|sitemap).*)"],
 };
